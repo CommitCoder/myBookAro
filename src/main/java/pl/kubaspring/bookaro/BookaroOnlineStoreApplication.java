c043412f0b18c560064de14b25e743ddc32db1ff
KubaKraken <k.jakub.dev.007@gmail.com>
2021-11-05 23:42:11 +0100

    Before Dependency injection (simple example)

--- src/main/java/pl/kubaspring/bookaro/BookaroOnlineStoreApplication.java
+++ src/main/java/pl/kubaspring/bookaro/BookaroOnlineStoreApplication.java
@@ -1,13 +1,26 @@
 package pl.kubaspring.bookaro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BookaroOnlineStoreApplication {
+public class BookaroOnlineStoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookaroOnlineStoreApplication.class, args);
 	}
 
+
+
+	// metoda ta uruchamia się w momencie gdy już cała apka wstanie (wstanie kontekst Spirnga, wstanie apka )
+	@Override
+	public void run(String... args) throws Exception {
+		CatalogService catalogService = new CatalogService();
+		List<Book> books = catalogService.findByTitle("Boy");
+		books.forEach(System.out::println);
+
+	}
 }
--- src/main/java/pl/kubaspring/bookaro/CatalogService.java
+++ src/main/java/pl/kubaspring/bookaro/CatalogService.java
@@ -1,4 +1,24 @@
 package pl.kubaspring.bookaro;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
 class CatalogService {
+
+    private final Map<Long, Book> storage = new ConcurrentHashMap<>();
+
+    public CatalogService() {
+        storage.put(1L, new Book(1L, "Potop", "Sienkiewicz",2022 ));
+        storage.put(2L, new Book(2L, "Boy", "Cezary",2021 ));
+        storage.put(3L, new Book(3L, "Patos", "Steve",2022 ));
+    }
+
+    List<Book> findByTitle(String title){
+        return storage.values()
+                .stream()
+                .filter(book -> book.title.startsWith(title))
+                .collect(Collectors.toList());
+    }
 }
